625891616ac8646c904e7e365596a6300071349e
jhoseph <jhosephdg@gmail.com>
2021-02-06 08:43:15 -0500

    finalizado ejercicio 1

---
+++ ejercicios/1.js
@@ -0,0 +1,10 @@
+// Implementar un algoritmo que reciba 2 argumentos y los sume, el resultado se
+// deberá imprimir en pantalla
+
+
+const num1 = 40;
+const num2 = 20;
+
+resultado = num1 + num2;
+
+console.log(resultado); 
